39c02117508facda3eaa12c3e8aa1f49610bc21e
shramee <shramee.srivastav@gmail.com>
2016-02-09 13:34:47 +0530

    Header banner area

--- functions.php
+++ functions.php
@@ -9,3 +9,15 @@ function ppdc_enqueue_scripts() {
 	wp_enqueue_style( 'divi-parent', $stylesheet );
 }
 add_action( 'wp_enqueue_scripts', 'ppdc_enqueue_scripts' );
+
+function ppdc_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Header Banner', 'Divi' ),
+		'id' => 'header-banner',
+		'before_widget' => '<div id="%1$s" class="et_pb_widget %2$s">',
+		'after_widget' => '</div> <!-- end .et_pb_widget -->',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	) );
+}
+add_action( 'widgets_init', 'ppdc_widgets_init' );
